73cff99c8281d5edb93134a55a8ee4e6720844b4
pi <pi@rpip>
2018-03-18 22:53:26 +0100

    a diff display

--- tix.py
+++ tix.py
@@ -5,15 +5,50 @@ import tix_lib
 from time import sleep
 import random
 import sys
+import parameters
+
 
 def signal_handler(signal, frame):
 	print("\033[?25h")
 	sys.exit(0)
 
-print("\033[?25l", end="")
-signal.signal(signal.SIGINT, signal_handler)
-while True:
-	g=tix_lib.get_time()
-	tix_lib.update(g[0] % 12, g[1])
-	sleep(random.randrange(0,10)/10+2.5)
-	tix_lib.go_back_up_three_lines()
+def run():
+	print("\033[?25l", end="") # remove cursor
+	signal.signal(signal.SIGINT, signal_handler)
+	blocks=None
+	time=None
+	inc=0
+	while True:
+		time,changed=tix_lib.get_time(old_time=time, format_=12)
+		columns_to_update=[]
+		if parameters.method==2:
+			if 0 in changed:
+				columns_to_update.append(0)
+				columns_to_update.append(1)
+			if 1 in changed:
+				columns_to_update.append(2)
+				columns_to_update.append(3)
+			if changed==[]:
+				if inc==0:
+					if time[0]<10:
+						inc=inc+1
+				if inc==2:
+					if time[1]<10:
+						inc=inc+1
+				columns_to_update=[inc]
+				
+				inc=(inc+1)%4
+			delay=0.5
+
+		if parameters.method==1:
+			columns_to_update=[0,1,2,3] 
+			delay=random.randrange(0,10)/10+2.5
+
+		s=""
+		for column in columns_to_update: 
+			blocks=tix_lib.update(time[0], time[1], blocks, column)
+			s=s+tix_lib.print_block(column, blocks[column])
+		print(s, end="") 
+		sleep(delay) 
+
+run()
--- tix_lib.py
+++ tix_lib.py
@@ -1,67 +1,104 @@
 import datetime
 import random
+import parameters
 
-use_256_colors=True
-
-tab="    " # at the beginning of the clock for each line
-space="  " # between blocks
-led="  " # size of a led
-led_space=" " # space between leds
-lines=1 # space between lines
-if use_256_colors:
+if parameters.use_256_colors:
 	colors=[124, 34, 21, 124]
-	colors=["\033[48;5;%dm" % c for c in colors]
-	empty_cell_color="\033[48;5;233m"
+	esc_colors=["\033[48;5;%dm" % c for c in colors]
+	esc_empty_cell_color="\033[48;5;233m"
 else:
 	colors=[41, 42, 44, 41]
-	colors=["\033[%dm" % c for c in colors]
-	empty_cell_color="\033[40m"
+	esc_colors=["\033[%dm" % c for c in colors]
+	esc_empty_cell_color="\033[40m"
 
+# init 
+block_str=""
+for i in range(0, parameters.led_width):
+	block_str=block_str+" "
 
 def get_array(val, cols):
 	c=[i for i in range(0,3*cols)]
 	d=[]
 	for i in range(0,val):
 		d.append(c.pop(random.randrange(len(c))))
-	return {'val': d, 'size': cols}
+	return d 
 
+def update(hour, minute, blocks, section_to_refresh):
+	if blocks==None:
+		blocks=[None, None, None, None]
+	if section_to_refresh==None or section_to_refresh==0:
+		blocks[0]=get_array(int(hour/10), 1)
+	if section_to_refresh==None or section_to_refresh==1:
+		blocks[1]=get_array(hour%10, 3)
+	if section_to_refresh==None or section_to_refresh==2:
+		blocks[2]=get_array(int(minute/10), 2)
+	if section_to_refresh==None or section_to_refresh==3:
+		blocks[3]=get_array(minute%10, 3)
 
-def update(hour, minute): 
+	return blocks	
 
-	blocks=[]
-	blocks.append(get_array(int(hour/10), 1))
-	blocks.append(get_array(hour%10, 3))
-	blocks.append(get_array(int(minute/10), 2))
-	blocks.append(get_array(minute%10, 3))
+#	for i in range(0, 3):
+#		for l in range(0, lines):
+#			print("")
+#		print(tab, end="")
+#		for j in range(0, len(blocks)):
+#			b=blocks[j]
+#			color=colors[j]
+#			for k in range(0, b['size']):
+#				l=k+i*b['size']
+#				if l in b['val']:
+#					print("%s%s" % (color, led), end="")
+#				else:
+#					print("%s%s" % (empty_cell_color, led), end="")
+#				print("\033[40m%s" % led_space, end="")
+#				#print( )
+#			print("\033[40m%s" % space, end="")
+#		print("\33[0m")
+#	return blocks
 
-	for i in range(0, 3):
-		for l in range(0, lines):
-			print("")
-		print(tab, end="")
-		for j in range(0, len(blocks)):
-			b=blocks[j]
-			color=colors[j]
-			for k in range(0, b['size']):
-				l=k+i*b['size']
-				if l in b['val']:
-					print("%s%s" % (color, led), end="")
-				else:
-					print("%s%s" % (empty_cell_color, led), end="")
-				print("\033[40m%s" % led_space, end="")
-				#print( )
-			print("\033[40m%s" % space, end="")
-		print("\33[0m")
-	return blocks
+def print_block(section, block):
+	# the cursor should be at the top left location before and after that function
+	block_left=0
+#block_top is 0
+	lines_down=0
+	output=""
+	for i in range(0, section): # calculating space before top left of the block to print
+		section_width=parameters.section_widths[i]
+		block_left=block_left+section_width*parameters.led_width+(section_width*parameters.led_space)+parameters.block_vert_space
+	s=""
+	section_width=parameters.section_widths[section]
+	for i in range(0, 3): # for each line
+		for j in range(0, section_width):
+			val=section_width*i+j
+			if val in block:
+				color=esc_colors[section]
+			else:
+				color=esc_empty_cell_color
+			left=block_left+j*(parameters.led_width + parameters.led_space)
+			output=output+color+"\033[%dG" % left
+			output=output+"%s%s" % (color, block_str)
+		output=output+"\n"
+		lines_down=lines_down+1
+		for i in range(0, parameters.block_horz_space):
+			output=output+"\n"
+			lines_down=lines_down+1 
+	# we got to put the cursor n lines up now
+	output=output+("\033[0m\033[%dF" % lines_down) 
+	return output
 
-
-def get_time():
+def get_time(old_time, format_):
 	now=datetime.datetime.now()
 	hour=now.hour
 	minute=now.minute
-	return [hour, minute]
-
-def go_back_up_three_lines():
-	a=3*(1+lines)
-	print("\033[%dA" % a, end="")
-
+	if format_==12:
+		hour=hour % 12
+	changed=[]
+	if old_time!=None:
+		if old_time[0] != hour:
+			changed.append(0)
+		if old_time[1] != minute:
+			changed.append(1)
+	else:
+		changed=[0,1,2,3]
+	return [[hour, minute], changed]
 
