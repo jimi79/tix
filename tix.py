1bf71aec89135c78826b6ad4d67dc48603da5d77
pi <pi@rpip>
2018-03-19 14:56:57 +0100

    removed the ability to change style, they were all bad. recenter automatically on the next refresh

--- tix.py
+++ tix.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 import signal
-import tix_lib
+from tix_lib import *
 from time import sleep
 import random
 import sys
@@ -10,7 +10,7 @@ import parameters
 
 def signal_handler(signal, frame):
 	print("\033[?25h")
-	print("\033[%dE" % 3*(parameters.block_vert_space+1))
+	#print("\033[%dE" % 3*(parameters.block_vert_space+1))
 	sys.exit(0)
 
 def run():
@@ -20,42 +20,8 @@ def run():
 	time=None
 	inc=0
 	while True:
-		time,changed=tix_lib.get_time(old_time=time, format_=12)
-		columns_to_update=[]
-		if parameters.style in [2, 3]:
-			if 0 in changed:
-				columns_to_update.append(0)
-				columns_to_update.append(1)
-			if 1 in changed:
-				columns_to_update.append(2)
-				columns_to_update.append(3)
-			if changed==[]:
-				if parameters.style==3:
-# for that style, we don't update empty blocked
-					if inc==0:
-						if time[0]<10:
-							inc=inc+1
-					if inc==2:
-						if time[1]<10:
-							inc=inc+1
-				columns_to_update=[inc]
-				
-				inc=(inc+1)%4
-			delay=0.5
-
-		s="" # string to print
-
-		if parameters.style==1:
-			columns_to_update=[0,1,2,3] 
-			delay=random.randrange(0,10)/10+2.5
-			s=s+"\033[2J"
-
-		s=s+"\033[0;0H" # we put cursor top left
-
-		for column in columns_to_update: 
-			blocks=tix_lib.update(hour=time[0], minute=time[1], blocks=blocks, section_to_refresh=column, add=parameters.style==3)
-			s=s+tix_lib.print_block(column, blocks[column])
-		print(s, end="") 
-		sleep(delay) 
+		time, blocks, stdout, delay=update_display(time, blocks)
+		print(stdout, end="")
+		sleep(delay)
 
 run()
--- tix_lib.py
+++ tix_lib.py
@@ -1,6 +1,7 @@
 import datetime
 import random
 import parameters
+import shutil
 
 if parameters.use_256_colors:
 	colors=[124, 34, 21, 124]
@@ -16,6 +17,18 @@ block_str=""
 for i in range(0, parameters.led_width):
 	block_str=block_str+" "
 
+def get_center():
+	a=shutil.get_terminal_size()
+	c=a.columns
+	l=a.lines
+	s=parameters.section_widths
+	w=sum(s) * (parameters.led_width + parameters.led_space) # the width of all leds
+	w=w + len(s) * parameters.block_horz_space # space between blocks
+	h=3 * (parameters.block_vert_space + 1) # block size is globally fixed vertically
+	X=int(c/2-w/2)
+	Y=int(l/2-h/2)
+	return {'X':X, 'Y' :Y}
+
 def get_array(val, cols):
 	c=[i for i in range(0,3*cols)]
 	d=[]
@@ -28,29 +41,27 @@ def get_cursor_pos_so_that_clock_is_centered():
 # i have to get the screen's size, maybe each time
 	pass
 
-def update(hour, minute, blocks, section_to_refresh, add):
+def update(hour, minute, blocks, section_to_refresh):
 	if blocks==None:
 		blocks=[None, None, None, None]
 	if section_to_refresh==None or section_to_refresh==0:
-		blocks[0]=get_array(int(hour/10) + add, parameters.section_widths[0])
+		blocks[0]=get_array(int(hour/10), parameters.section_widths[0])
 	if section_to_refresh==None or section_to_refresh==1:
-		blocks[1]=get_array(hour%10 + add, parameters.section_widths[1])
+		blocks[1]=get_array(hour%10, parameters.section_widths[1])
 	if section_to_refresh==None or section_to_refresh==2:
-		blocks[2]=get_array(int(minute/10) + add, parameters.section_widths[2])
+		blocks[2]=get_array(int(minute/10), parameters.section_widths[2])
 	if section_to_refresh==None or section_to_refresh==3:
-		blocks[3]=get_array(minute%10 + add, parameters.section_widths[3]) 
+		blocks[3]=get_array(minute%10, parameters.section_widths[3]) 
 	return blocks	
 
-def print_block(section, block):
-	# the cursor should be at the top left location before and after that function
-	block_left=0
-#block_top is 0
-	lines_down=0
+def print_block(section, block, X, Y):
 	output="" # we clear the screen
 	for i in range(0, section): # calculating space before top left of the block to print
 		section_width=parameters.section_widths[i]
-		block_left=block_left+section_width*parameters.led_width+(section_width*parameters.led_space)+parameters.block_vert_space
-	s=""
+		X=X+section_width*parameters.led_width+(section_width*parameters.led_space)+parameters.block_vert_space
+	output="\033[%d;1H" % (Y+1) 
+
+	count_added_lines=0 # number of 
 	section_width=parameters.section_widths[section]
 	for i in range(0, 3): # for each line
 		for j in range(0, section_width):
@@ -59,17 +70,27 @@ def print_block(section, block):
 				color=esc_colors[section]
 			else:
 				color=esc_empty_cell_color
-			left=block_left+j*(parameters.led_width + parameters.led_space)
+			left=X+j*(parameters.led_width + parameters.led_space)
 			output=output+color+"\033[%dG" % left
 			output=output+"%s%s" % (color, block_str)
 		output=output+"\n"
-		lines_down=lines_down+1
+		count_added_lines=count_added_lines+1
 		for i in range(0, parameters.block_horz_space):
 			output=output+"\n"
-			lines_down=lines_down+1 
+			count_added_lines=count_added_lines+1 
 	# we got to put the cursor n lines up now
-	output=output+("\033[0m\033[%dF" % lines_down) 
+	#output=output+("\033[%dF" % count_added_lines)  # might not be necessary, not sure though
+	output=output+("\033[0m") 
 	return output
+
+def print_blocks(time, blocks, X, Y): 
+#def update(hour, minute, blocks, section_to_refresh, add):
+	blocks=update(time[0], time[1], blocks, None)
+	s="\033[2J"
+	s=s+"\033[0;0H" # we put cursor top left, we'll change that later
+	for i, block in zip(range(0, len(blocks)), blocks):
+		s=s+print_block(i, block, X, Y)
+	return s
 
 def get_time(old_time, format_):
 	now=datetime.datetime.now()
@@ -86,4 +107,14 @@ def get_time(old_time, format_):
 	else:
 		changed=[0,1,2,3]
 	return [[hour, minute], changed]
+
+
+def update_display(time, blocks): 
+	delay=random.randrange(0,10)/10+2.5 
+	time, changed=get_time(old_time=time, format_=12)
+	center=get_center()
+	stdout=print_blocks(time, blocks, center['X'], center['Y']) 
+	return time, blocks, stdout, delay 
+	
+
 
