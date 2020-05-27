10d766521b66ff25a5a08e20d1f5cc8f6208ebab
pi <pi@lorem>
2020-05-27 14:04:58 +0200

    reformat code and add 24h support

--- tix.py
+++ tix.py
@@ -6,22 +6,28 @@ from time import sleep
 import random
 import sys
 import parameters
+import argparse
 
 
 def signal_handler(signal, frame):
-	print("\033[?25h")
-	#print("\033[%dE" % 3*(parameters.block_vert_space+1))
-	sys.exit(0)
+    print("\033[?25h")
+    #print("\033[%dE" % 3*(parameters.block_vert_space+1))
+    sys.exit(0)
 
-def run():
-	print("\033[?25l", end="") # remove cursor
-	signal.signal(signal.SIGINT, signal_handler)
-	blocks=None
-	time=None
-	inc=0
-	while True:
-		time, blocks, stdout, delay=update_display(time, blocks)
-		print(stdout, end="")
-		sleep(delay)
+def run(h24):
+    print("\033[?25l", end = "") # remove cursor
+    signal.signal(signal.SIGINT, signal_handler)
+    blocks=None
+    time=None
+    inc=0
+    while True:
+        time, blocks, stdout, delay=update_display(time, blocks, h24)
+        print(stdout, end = "")
+        sleep(delay)
 
-run()
+
+parser = argparse.ArgumentParser(description = 'tix clock')
+parser.add_argument('--24', action = 'store_const', const = True, default = False) 
+args = parser.parse_args()
+print(args)
+run(h24 = args.__getattribute__('24'))
